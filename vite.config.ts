9c37e8a87f2a6c21cf9ae828c006448ee6ae4648
yevhenii vorobiov <yevhenii.vorobiov@bilton.tech>
2025-06-17 13:54:05 +0300

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import path from 'path';
+import path from "path";
 
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/event-guru/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@redux': path.resolve(__dirname, './src/redux'),
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@redux": path.resolve(__dirname, "./src/redux"),
     },
   },
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
