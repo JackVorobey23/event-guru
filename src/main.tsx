fc9cb7630835aa2552db5e955ac4bdde22d04612
yevhenii vorobiov <yevhenii.vorobiov@bilton.tech>
2025-06-17 14:38:10 +0300

    fix: routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
-import { Provider } from 'react-redux';
-import { store } from '@redux/store.ts';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
+import { store } from "@redux/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/event-guru">
         <App />
       </BrowserRouter>
     </Provider>
